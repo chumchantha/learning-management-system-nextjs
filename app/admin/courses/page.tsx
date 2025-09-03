d88309979141b8c455a0007bb5ee67931ade1d32
Chum Chantha <chumchantha22y@gmail.com>
2025-09-03 16:11:21 +0700

    feat: add courses page

--- app/admin/courses/page.tsx
+++ app/admin/courses/page.tsx
@@ -1,7 +1,44 @@
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 import React from "react";
 
 const Courses = () => {
-  return <div>All Courses</div>;
+  return (
+    <>
+      <div className="flex items-center justify-between mb-6">
+        <h1 className="text-2xl font-bold">Your Courses</h1>
+        <Link
+          href="/admin/courses/create"
+          className={buttonVariants({ variant: "secondary" })}
+        >
+          Create Course
+        </Link>
+      </div>
+      <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        <Card>
+          <CardHeader>
+            <CardTitle>Course 1</CardTitle>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Course 1</CardTitle>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Course 1</CardTitle>
+          </CardHeader>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Course 1</CardTitle>
+          </CardHeader>
+        </Card>
+      </div>
+    </>
+  );
 };
 
 export default Courses;
