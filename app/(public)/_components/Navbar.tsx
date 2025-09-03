9795a0e06203ad76f1fbb9e4b87cb6d0cbb3ff5e
Chum Chantha <chumchantha22y@gmail.com>
2025-09-03 13:48:13 +0700

    refactor(ui): update ui, add navbar

---
+++ app/(public)/_components/Navbar.tsx
@@ -0,0 +1,79 @@
+"use client";
+import { buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/ui/themeToggle";
+import { authClient } from "@/lib/auth-client";
+import Link from "next/link";
+import React from "react";
+import UserDropdown from "./UserDropdown";
+
+const navigationItems = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Courses",
+    path: "/courses",
+  },
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+  },
+];
+
+const Navbar = () => {
+  const { data: session, isPending } = authClient.useSession();
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/80 backdrop-blur-sm">
+      <div className="container min-h-14 mx-auto flex gap-4 items-center justify-between px-4 md:px-6 lg:px-8 ">
+        <Link href="/">
+          <h1 className="text-2xl font-semibold tracking-wide">SoyVibeCoder</h1>
+        </Link>
+
+        {/* Desktop Nav */}
+        <nav className="hidden md:flex md:flex-1 md:items-center md:justify-between">
+          <div className="flex items-center justify-center gap-4">
+            {navigationItems.map((item) => {
+              return (
+                <div key={item.name}>
+                  <Link href={item.path}>{item.name}</Link>
+                </div>
+              );
+            })}
+          </div>
+        </nav>
+
+        {/* Mobile Nav  */}
+        <nav>
+          <div className="flex gap-2 items-center justify-center">
+            <ThemeToggle />
+            {isPending ? null : session ? (
+              <UserDropdown
+                name={session.user.name}
+                email={session.user.email}
+                image={session.user.image || ""}
+              />
+            ) : (
+              <>
+                <Link
+                  href="/login"
+                  className={buttonVariants({ variant: "secondary" })}
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/login"
+                  className={buttonVariants({ variant: "default" })}
+                >
+                  Get Started
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,7 +1,15 @@
+import Navbar from "@/app/(public)/_components/Navbar";
 import React from "react";
 
 const PublicLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="max-w-7xl mx-auto px-4">{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      <main className="container max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
+        {children}
+      </main>
+    </div>
+  );
 };
 
 export default PublicLayout;
